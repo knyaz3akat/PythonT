34bb5714732b19450b49e23e68f5d11fe3048b93
zakat <s.n.golubov@gmail.com>
2022-11-23 14:04:55 +0300

    2 задание исправление по замечаниям. Исключены дублирующиеся файлы

--- group1.py
+++
@@ -1,5 +0,0 @@
-class Group1:
-    def __init__(self, gname, gheader, gfooter):
-        self.gname = gname
-        self.gheader = gheader
-        self.gfooter = gfooter
--- test_add_group.py
+++ test_add_group.py
@@ -8,72 +8,68 @@ from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 from group import Group
 
-class test_add_group(unittest.TestCase):
+class test1_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_add_group(self):
+    def test1_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.creat_group(wd, Group(name="qwe2", header="qqq2", footer="www2"))
-        self.return_to_group_page(wd)
+        self.creat_group(wd, Group(gname="gr1", gheader="qwerty gr1", gfooter="asdfgh qr1"))
         self.logout(wd)
 
-    def test_add_empty_group(self):
+    def test1_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.creat_group(wd, Group(name="", header="", footer=""))
-        self.return_to_group_page(wd)
+        self.creat_group(wd, Group(gname="", gheader="", gfooter=""))
         self.logout(wd)
 
     def logout(self, wd):
-        # logout
+        # Exit
         wd.find_element_by_link_text("Logout").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
 
     def return_to_group_page(self, wd):
-        # return to group page
+        # Return to group page
         wd.find_element_by_link_text("group page").click()
 
-    def creat_group(self, wd, group):
-        # init group creation
+    def creat_group(self, wd, group1):
+        self.open_group_page(wd)
+        # Creat new group
         wd.find_element_by_name("new").click()
-        # fill group form
+        # Fill group
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_name").send_keys(group1.gname)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_header").send_keys(group1.gheader)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
+        wd.find_element_by_name("group_footer").send_keys(group1.gfooter)
+        # Submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_group_page(wd)
 
     def open_group_page(self, wd):
-        # open group page
+        # Open group page
         wd.find_element_by_link_text("groups").click()
 
+    def open_home_page(self, wd):
+        # Open home page
+        wd.get("http://localhost/addressbook/")
+
     def login(self, wd, username, password):
-        # login
+        self.open_home_page(wd)
+        # Login
         wd.find_element_by_name("user").click()
-        wd.find_element_by_xpath("//form[@id='LoginForm']/label[2]").click()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_id("LoginForm").click()
+        # Authorization
         wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        # open home page
-        wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
@@ -88,5 +84,4 @@ class test_add_group(unittest.TestCase):
     def tearDown(self):
         self.wd.quit()
 
-if __name__ == "__main__":
-    unittest.main()
+
