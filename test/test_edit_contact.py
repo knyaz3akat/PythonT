73e3e87975faff88894fae22511e014df7bc077d
zakat <s.n.golubov@gmail.com>
2022-11-24 21:39:01 +0300

    7 задание. Выделены два метода при создании контакта: 1- заполнение основной информации, 2- вторичной информации. Оба метода используются для создания контакта и редактировании первого контакта в списке.

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,7 +2,7 @@ from model.contact import Contact
 
 def test_edit_first_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.edit_first_contact(Contact(firstname="Fname001", middlename="Mname001", lastname="Lname001", nickname="Nname001",
+    app.contact.edit_first_contact(Contact(firstname="Fname003", middlename="Mname003", lastname="Lname003", nickname="Nname003",
                                            title="qwerty001", company="asdfgh002", address="zxcvbnm003", homephone="111000222", mobile="111000333",
                                            workphone="111000444", fax="111000555", email="asd01@asd.asd", email2="asd02@asd.ad", email3="asd03@asd.asd",
                                            homepage="www.ya1.ru", bday="8", bmonth="March", byear="1980", aday="10", amonth="July",
