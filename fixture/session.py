e05e44b588d8aabb246468c9bbd6b97678471ba9
zakat <s.n.golubov@gmail.com>
2022-11-25 03:07:53 +0300

    8 добавили проверку на предусловия выполнеия тестов (авторизации на сайте под нужным пользователем)

--- fixture/session.py
+++ fixture/session.py
@@ -21,3 +21,24 @@ class SessionHelper:
         wd.find_element_by_link_text("Logout").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else: self.logout()
+        self.login(username, password)
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wb.find_element_by_xpath("xpath=//div[@id='top']/form/b").text == "("+username+")"
