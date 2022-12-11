2710faf33f7e655722bd85c6fb273d8e1deed06f
zakat <s.n.golubov@gmail.com>
2022-12-12 02:55:57 +0300

    8 добавили проверку на предусловия выполнеия тестов (авторизации на сайте под нужным пользователем) упростили метод заполнения полей контакта через конструкцию если-то-иначе

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,12 +1,12 @@
 from model.contact import Contact
-"""
+
 def test_edit_first_contact(app):
     app.contact.edit_first_contact(Contact(firstname="Fname003", middlename="Mname003", lastname="Lname003", nickname="Nname003",
                                            title="qwerty001", company="asdfgh002", address="zxcvbnm003", homephone="111000222", mobile="111000333",
                                            workphone="111000444", fax="111000555", email="asd01@asd.asd", email2="asd02@asd.ad", email3="asd03@asd.asd",
                                            homepage="www.ya1.ru", bday="8", bmonth="March", byear="1980", aday="10", amonth="July",
                                            ayear="2010", new_group="", address2="zxz000xzx", phone2="cvc000vcv", notes="bnb000nbn"))
-"""
+
 
 def test_edit_first_contact_fname(app):
     app.contact.edit_first_contact(Contact(firstname="Fname004"))
