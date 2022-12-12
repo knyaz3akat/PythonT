fde9ca236a115f2dcf2acb5e84e88bc28cbdaae0
zakat <s.n.golubov@gmail.com>
2022-12-12 03:15:02 +0300

    9 добавили предусловие на проверку наличия хотя бы одной записи для удаления или редактирования групп/контактов

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,13 +1,19 @@
 from model.group import Group
 
 def test_edit_first_group(app):
+    if app.group.count()==0:
+        app.group.create(Group(gname="GN_test"))
     app.group.edit_first_group(Group(gname="gr3-edit", gheader="qwerty gr3-edit", gfooter="asdfgh qr3-edit"))
 
 
 def test_edit_group_name(app):
+    if app.group.count()==0:
+        app.group.create(Group(gname="GN_test"))
     app.group.edit_first_group(Group(gname="New gr3-edit"))
 
 
 def test_edit_group_header(app):
+    if app.group.count()==0:
+        app.group.create(Group(gname="GN_test"))
     app.group.edit_first_group(Group(gheader="New qwerty"))
 
