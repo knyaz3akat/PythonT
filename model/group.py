277537b7848548554bef4bc3655956e06d433b75
zakat <s.n.golubov@gmail.com>
2022-12-13 14:02:47 +0300

    11 добавили сравнения списка групп

--- model/group.py
+++ model/group.py
@@ -1,6 +1,20 @@
+from sys import maxsize
+
 class Group:
     def __init__(self, gname=None, gheader=None, gfooter=None, id=None):
         self.gname = gname
         self.gheader = gheader
         self.gfooter = gfooter
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.gname)
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and self.gname==other.gname
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,12 +3,15 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count()==0:
         app.group.create(Group(gname="GN_test"))
-
     old_groups=app.group.get_group_list()
-    app.group.edit_first_group(Group(gname="gr3-edit", gheader="qwerty gr3-edit", gfooter="asdfgh qr3-edit"))
+    group=Group(gname="gr3-edit", gheader="qwerty gr3-edit", gfooter="asdfgh qr3-edit")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-
+    old_groups[0]=group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+"""
 def test_edit_group_name(app):
     if app.group.count()==0:
         app.group.create(Group(gname="GN_test"))
@@ -24,3 +27,4 @@ def test_edit_group_header(app):
     app.group.edit_first_group(Group(gheader="New qwerty"))
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+"""
